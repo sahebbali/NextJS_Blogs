e01d7a43a9fb293deb8bf121c49beaf37ea35862
sahebbali <sahebbali253@gmail.comm>
2023-06-18 16:33:18 +0600

    problem

--- app/page.jsx
+++ app/page.jsx
@@ -5,17 +5,21 @@ import fetchBlogs from '../components/FetchBlogs'
 
 
 export default async function Home() {
-  const blogs = await fetchBlogs();
+  // const blogs = await fetchBlogs();
 
   return (
    <div className={classes.container}>
     {/* {blogs?.length > 0 && <h2>WebDevMania&apos;s Blog Website</h2>} */}
-     <div className={classes.wrapper}>
+     {/* <div className={classes.wrapper}>
       {blogs?.length > 0 
        ? blogs.map((blog) => (
         <BlogCard key={blog._id} blog={blog}/>
       )) : <h3 className={classes.noBlogs}>No blogs are currently in the app</h3>}
-     </div>
+     </div> */}
+
+     <div> <h1>
+        THis is for Error selection Div
+      </h1></div>
    </div>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
       appDir: true,
       serverComponentsExternalPackages: ["mongoose"],
     },
+    productionBrowserSourceMaps: true,
     images: {
       remotePatterns: [
           { hostname: 'res.cloudinary.com', protocol: 'https', port: '' }
